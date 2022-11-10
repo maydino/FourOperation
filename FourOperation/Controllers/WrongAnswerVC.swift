da999aad1da10306b4f1812e10a58413e679144c
maydino <mutluaydin.m@gmail.com>
2022-11-09 22:15:38 -0800

    PlusVC UI Improbemnts

---
+++ FourOperation/Controllers/WrongAnswerVC.swift
@@ -0,0 +1,22 @@
+//
+//  wrongAnswerVCViewController.swift
+//  FourOperation
+//
+//  Created by Mutlu Aydin on 11/9/22.
+//
+
+import UIKit
+
+class wrongAnswerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        
+    }
+    
+
+    
+
+}
