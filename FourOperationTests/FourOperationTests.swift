f7919c960bb1ceae0bf8b3a239355753af2eae8f
maydino <mutluaydin.m@gmail.com>
2022-11-22 21:43:58 -0800

    Add unit tests to new setup. Add app logo

---
+++ FourOperationTests/FourOperationTests.swift
@@ -0,0 +1,20 @@
+//
+//  FourOperationTests.swift
+//  FourOperationTests
+//
+//  Created by Mutlu Aydin on 11/22/22.
+//
+
+import XCTest
+@testable import FourOperation
+
+final class FourOperationTests: XCTestCase {
+
+
+    func test_maths_addingTwoPlusThree_equalsFive(){
+        
+        let plusVC = PlusVC()
+    }
+    
+
+}
